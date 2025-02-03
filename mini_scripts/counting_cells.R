b2203177de8514c6e8abab3890bf28e52cd32608
nehaprs <nehaprs@gmail.com>
2025-02-03 17:50:38 -0500

    confidence in cell type prediction

--- mini_scripts/counting_cells.R
+++ mini_scripts/counting_cells.R
@@ -25,3 +25,18 @@ s.data <- readRDS("~/BINF/yushi scrnaseq/E11.5/sox9/ref_annot/sox115.rds")
 cell.count = as.data.frame(table(s.data$predicted.id))
 colnames(cell.count) = c("cell type", "number of cells")
 write_xlsx(cell.count, "sox9e115cellcount.xlsx")
+
+#finding confidence level of cell-level annotation
+setwd("~/BINF/yushi scrnaseq/New folder/cell_counts/cell counts/cell count confidence")
+s.data <- readRDS("~/BINF/yushi scrnaseq/E9.5/Pax3/ref_annot/pax95.rds") 
+
+predicted_scores = s.data@meta.data[, c("predicted.id", "prediction.score.max")]
+
+
+confidence_summary = predicted_scores %>%
+  group_by(predicted.id) %>%
+  summarize(Avg_Pred_Score = mean(prediction.score.max, na.rm = TRUE),
+            Median_Pred_score = median(prediction.score.max, na.rm = TRUE),
+            Cell_Count = n())
+
+write_xlsx(as.data.frame(confidence_summary),"pax3e95_predscore.xlsx")
