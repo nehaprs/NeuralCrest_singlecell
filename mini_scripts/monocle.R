111a7007a4fa510bd98c178c731a39c78d40ea1c
nehaprs <nehaprs@gmail.com>
2025-04-29 12:09:13 -0400

    Update monocle.R

--- mini_scripts/monocle.R
+++ mini_scripts/monocle.R
@@ -21,4 +21,26 @@ root_cells = WhichCells(s.processed,
 
 
 root_clusters = unique(s.processed$cell_state[root_cells])
+cds = as.cell_data_set(s.processed)
+cds <- cluster_cells(cds, reduction_method = "UMAP")
+colData(cds)$cluster <-s.processed$cell_state
+cds <- learn_graph(cds, use_partition = FALSE)
+cds <- order_cells(cds, root_cells = root_cells)
+plot_cells(
+  cds,
+  color_cells_by = "pseudotime",
+  label_groups_by_cluster = FALSE,
+  label_roots = FALSE,
+  label_branch_points = FALSE,
+  label_leaves  = FALSE,
+  label_cell_groups = FALSE
+)
+
+pseudotime(cds)
+cds$monocle3_pseudotime <- pseudotime(cds)
+data.pseudo <- as.data.frame(colData(cds))
+
+s.processed$pseudotime <- pseudotime(cds)
+Idents(s.processed) <-s.processed$pseudotime
+FeaturePlot(s.processed, features = "pseudotime", label = F)
 
