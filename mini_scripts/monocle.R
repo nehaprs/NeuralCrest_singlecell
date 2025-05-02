b0cfd7d23f004b0db73c71ed7a4626803563132f
nehaprs <nehaprs@gmail.com>
2025-05-02 12:11:34 -0400

    Update monocle.R

--- mini_scripts/monocle.R
+++ mini_scripts/monocle.R
@@ -17,9 +17,75 @@ Idents(s.processed)= s.processed@meta.data$cell_state
 DimPlot(s.processed) +NoLegend()
 #pre-process and do partitions
 cds = as.cell_data_set(s.processed)
+
+# to get cell metadata
+colData(cds)
+# to gene metdata
+fData(cds)
+rownames(fData(cds))[1:10]
+
+# since it misses the gene_short_name column, let's add it
+fData(cds)$gene_short_name <- rownames(fData(cds))
+
+
+# to get counts
+counts(cds)
+
+
+# Assign the cluster info 
+
+list_cluster <- s.processed@active.ident
+cds@clusters$UMAP$clusters <- list_cluster
+
+
+# Assign UMAP coordinate - cell embeddings
+
+cds@int_colData@listData$reducedDims$UMAP <-  s.processed@reductions$umap@cell.embeddings
+
+
+# plot
+
+cluster.before.trajectory <- plot_cells(cds,
+                                        color_cells_by = 'cluster',
+                                        label_groups_by_cluster = FALSE) +
+  NoLegend()
+
+
+
+cluster.names <- plot_cells(cds,
+                            color_cells_by = "cell_state",
+                            label_groups_by_cluster = FALSE) +
+  NoLegend()
+
+
+
+# ...3. Learn trajectory graph ------------------------
+cds <- learn_graph(cds, use_partition = FALSE)
+
+plot_cells(cds,
+           color_cells_by =  "cell_state",
+           label_groups_by_cluster = FALSE,
+           label_branch_points = FALSE,
+           label_roots = FALSE,
+           label_leaves = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################
 #cds <- preprocess_cds(cds, num_dim = 30)
 #cds <- reduce_dimension(cds, reduction_method = "UMAP")
-cds <- cluster_cells(cds, reduction_method = "UMAP", k = 100)
+cds <- cluster_cells(cds, reduction_method = "UMAP")
 
 cds@clusters$UMAP$partitions
 
@@ -29,7 +95,7 @@ cds@clusters$UMAP$partitions
 plot_cells(cds, color_cells_by = "partition", label_groups_by_cluster = TRUE)
 
 
-cds <- learn_graph(cds, use_partition = TRUE)
+cds <- learn_graph(cds, use_partition =FALSE)
 cds <- order_cells(cds, root_cells = root_cells)
 plot_cells(
   cds,
