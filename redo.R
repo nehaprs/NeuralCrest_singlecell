5551238de57b024df64ba11143854fd7ebef01ec
nehaprs <nehaprs@gmail.com>
2025-11-06 16:10:39 -0500

    Add initial redo.R script with library imports
    Created redo.R script that sets the working directory and loads required R libraries for single-cell RNA-seq analysis.

---
+++ redo.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(writexl)
+library(readxl)
+library(clustree)
+library(hdf5r)
+library(scCATCH)
+library(tidyr)  
+library(harmony)
+
+setwd("~/BINF/yushi scrnaseq/redo")
+
